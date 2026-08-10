230ff609ba505570c9699caf1a543de3efd422f8
sirmufasas <sirmufasas@users.noreply.github.com>
2026-08-10 17:53:34 +0000

    Allow preview hosts in vite server/preview config

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,15 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: true,
     hmr: {
       overlay: false,
     },
+  },
+  preview: {
+    host: "::",
+    port: 8080,
+    allowedHosts: true,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
